0d6c74314a04852f33fd76d59df12620adb4833f
zeroskillsamus <abemitch06500@hotmail.com>
2024-12-17 21:19:27 -0600

    created a route to fetch video

---
+++ simulate_hianime/api.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import fetch_video_src from './index.js'
+
+const HIANIME = express()
+const port = 3069
+
+HIANIME.listen(port, async () => {
+	console.log(`Opening On Port: ${port}`)
+})
+
+HIANIME.get('/ZORO/Fetch-Video/:episode_id', async (req, res) => {
+	let { episode_id } = req.params
+	let results = await fetch_video_src(episode_id)
+	return res.send(JSON.stringify(results, null, 4))
+})
